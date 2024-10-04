f0d491dbbbb25383152dc4eaa6c50eae46467a73
Paul Milke <paul@trainerroad.com>
2024-10-04 15:29:31 -0700

    adds initial class model

---
+++ SchoolPlanner.Data/Models/Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolPlanner.Data.Models
+{
+    class Class
+    {
+        public int ClassId { get; set; }
+        public int TermId { get; set; }
+        public required string ClassName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public required string Status { get; set; }
+        public required string InstructorName { get; set; }
+        public required string InstructorPhone { get; set; }
+        public required string InstructorEmail { get; set; }
+        public string? Notes {get; set; }
+    }
+}
