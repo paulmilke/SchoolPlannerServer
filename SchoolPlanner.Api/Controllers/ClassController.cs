c1c1cccc589308173e8f31a42ca455af0e0f4db4
Paul Milke <paul@trainerroad.com>
2024-10-06 16:59:36 -0700

    creates api class controller

---
+++ SchoolPlanner.Api/Controllers/ClassController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolPlanner.Data.Interfaces;
+using SchoolPlanner.Data.Models; 
+
+namespace SchoolPlanner.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClassController
+    {
+        private readonly IClassRepository _classRepository; 
+
+        public ClassController(IClassRepository classrepository)
+        {
+            _classRepository = classrepository; 
+        }
+
+        [HttpGet(Name = "GetClasses")]
+        public async Task<IEnumerable<Class>> Get(int termID)
+        {
+            var classes = await _classRepository.GetClassesAsync(termID);
+            return classes; 
+        }
+    }
+}
