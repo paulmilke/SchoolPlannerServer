271ed23d1db63733e12c5d7e3357cb8f9709772c
Paul Milke <paul@trainerroad.com>
2024-10-06 16:53:00 -0700

    creates interface and repo  for class db interaction

---
+++ SchoolPlanner.Data/Repositories/ClassRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolPlanner.Data.Interfaces;
+using SchoolPlanner.Data.Models; 
+
+namespace SchoolPlanner.Data.Repositories
+{
+    public class ClassRepository : IClassRepository
+    {
+        SchoolDBContext _dbContext;
+
+        public ClassRepository(SchoolDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Class>> GetClassesAsync(int termId)
+        {
+            var classes = await _dbContext.Classes.Where(c => c.TermId == termId).ToListAsync();
+            return classes; 
+        }
+    }
+}
