b41f8d1d2a0987ade46d62b25dfd8771447244fa
Paul Milke <milkepaul1@gmail.com>
2024-12-02 16:11:06 -0800

    adds comment

--- SchoolPlanner.UnitTests/TermControllerTests.cs
+++ SchoolPlanner.UnitTests/TermControllerTests.cs
@@ -19,7 +19,7 @@ namespace SchoolPlanner.UnitTests
             _termController = new TermController(_mockTermRepository.Object);
         }
 
-
+        //Tests against the terms controller for returning a list of terms. 
         [Fact]
         public async Task GetTerms_ShouldReturnOkResult()
         {
